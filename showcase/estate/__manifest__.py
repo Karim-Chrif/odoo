ba5a74e3942dd5b6de73f7ef3b44f4ff67726286
carim <carimshrif@gmail.com>
2025-03-10 02:48:25 +0100

    chapter 1->6

---
+++ showcase/estate/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Estate',
+    'version': '1.0',
+    'summary': 'tutorial',
+    'author': 'Your Name',
+    'category': 'Tools',
+    'depends': ['base'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml',
+    ],
+    'installable': True,
+    'application': True,
+    'license':'LGPL-3',
+}
---
+++ showcase/estate/models/estate_property.py
@@ -0,0 +1,32 @@
+from odoo import models, fields
+from odoo.tools.convert import relativedelta
+
+class EstateProperty(models.Model):
+    _name = 'estate.property'
+    _description = 'Estate Property'
+
+    name = fields.Char(string='Title', required=True)
+    description = fields.Text()
+    postcode = fields.Char()
+    date_availability = fields.Date(copy=False, default=lambda self: fields.Date.today() + relativedelta(months=3))
+    expected_price = fields.Float(required=True)
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
+    living_area = fields.Integer(string='Living Area (sqm)')
+    facades = fields.Integer()
+    garage = fields.Boolean()
+    garden = fields.Boolean()
+    garden_area = fields.Integer()
+    garden_orientation = fields.Selection([
+        ('north', 'North'),
+        ('south', 'South'),
+        ('east', 'East'),
+        ('west', 'West'),
+    ])
+    active = fields.Boolean(default=True)
+    state = fields.Selection([
+        ('new', 'New'),
+        ('offer_received', 'Offer Received'),
+        ('offer_accepted', 'Offer Accepted'),
+        ('sold', 'Sold'),
+    ], default='new', copy=False)
